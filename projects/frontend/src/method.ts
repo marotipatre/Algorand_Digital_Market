ab0577bea8249c7b9053342d39fb2d20d9debb55
marotipatre <vaibhavspatre@gmail.com>
2025-01-10 16:42:06 +0530

    smart contract interaction updated

--- projects/frontend/src/method.ts
+++ projects/frontend/src/method.ts
@@ -1,1 +1,8 @@
-import * as algokit from 
+import * as algokit from  '@algorandfoundation/algokit-utils';
+import { DigitalMarketClient , DigitalMarketFactory } from './contracts/DigitalMarket';
+
+export function create(algorand: algokit.AlgorandClient, dmClient: DigitalMarketFactory , assetBeingSolf: bigint, unitaryPrice: bigint) {
+  return async () => {
+    await dmClient.send.create.createApplication({ args: [assetBeingSolf, unitaryPrice] })
+  }}  
+
